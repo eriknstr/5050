df9abb9a3dde5a97c9d02d0eaea890458bf165a1
Bubble07 <support@degstu.com>
2016-11-21 19:02:50 -0500

    Create db_con.php

---
+++ db_con.php
@@ -0,0 +1,11 @@
+<?php
+//5050 user password: REDACTED
+//5050: REDACTED
+
+$db_con = mysqli_connect("localhost", "REDACTED", "REDACTED", "REDACTED");
+
+// Check connection
+if (mysqli_connect_errno()){
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+?>
